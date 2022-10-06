239cd7b5501468a2864ed28fe88a84e410c75c3f
Elias <eliasfurtado.dev@gmail.com>
2022-10-06 18:04:27 -0300

    create url DTO

---
+++ src/main/java/br/com/eliasfurtado/UrlShortener/dto/UrlLongRequest.java
@@ -0,0 +1,14 @@
+package br.com.eliasfurtado.UrlShortener.dto;
+
+public class UrlLongRequest {
+
+    private String LongUrl;
+
+    public String getLongUrl() {
+        return LongUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        LongUrl = longUrl;
+    }
+}
