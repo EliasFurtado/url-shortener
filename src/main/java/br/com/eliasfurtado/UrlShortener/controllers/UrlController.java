2ff53a2b3ccdb671196d7c4e383b760d655672ee
Elias <eliasfurtado.dev@gmail.com>
2022-10-06 18:25:01 -0300

    create get and post methods

--- src/main/java/br/com/eliasfurtado/UrlShortener/controllers/UrlController.java
+++ src/main/java/br/com/eliasfurtado/UrlShortener/controllers/UrlController.java
@@ -1,10 +1,34 @@
 package br.com.eliasfurtado.UrlShortener.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.eliasfurtado.UrlShortener.dto.UrlLongRequest;
+import br.com.eliasfurtado.UrlShortener.services.UrlService;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UrlController {
 
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+    @PostMapping("create-short")
+    public String convertToShortUrl(@RequestBody UrlLongRequest request) {
+        return urlService.convertToShortUrl(request);
+    }
+    @GetMapping(value = "{shortUrl}")
+    @Cacheable(value = "urls", key = "#shortUrl", sync = true)
+    public ResponseEntity<Void> getAndRedirect(@PathVariable String shortUrl) {
+        String url = urlService.getOriginalUrl(shortUrl);
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(url))
+                .build();
+    }
 }
